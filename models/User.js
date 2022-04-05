9edde1835e23bea4902411cfe84b4056d8cae13e
Andrew S <andrewsheamls@gmail.com>
2022-04-04 18:43:14 -0600

    user model created

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose"); //taken from lesson 21
+
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true, //taken from lesson 21
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", userSchema);
+
+model.exports = User;
