83e0cd86f8298f67e8e5e8e674cd7570d64bd945
Andrew S <andrewsheamls@gmail.com>
2022-04-04 20:14:42 -0600

    copied index.js from lesson 28

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,11 @@
 const router = require('express').Router();
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.status(404).send('Wrong route!')})
+
+module.exports = router;
+
+
+
